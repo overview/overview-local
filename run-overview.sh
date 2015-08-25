ab467729cd56e8560784522c10902ec0babb0afe
Jonas Karlsson <thejonas@gmail.com>
2015-08-25 10:50:49 -0400

    Start plugins

--- run-overview.sh
+++ run-overview.sh
@@ -3,3 +3,12 @@
 docker-compose -f config/services.yml up -d
 docker-compose -f config/db-setup.yml up
 docker-compose -f config/overview.yml up -d
+
+
+# The next release of docker-compose will allow environment
+# variables to be expanded in the yml file...
+echo DOCKER_HOST=$DOCKER_HOST > config/host.env
+
+docker-compose -f config/plugins.yml up -d
+
+
