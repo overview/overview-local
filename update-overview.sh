481f111f3e6c65f19058dfd53075b8d8f6778b2b
Jonas Karlsson <thejonas@gmail.com>
2015-08-27 14:55:16 -0400

    Update evolutions

--- update-overview.sh
+++ update-overview.sh
@@ -4,5 +4,6 @@ docker-compose -f config/services.yml stop
 docker-compose -f config/overview.yml stop
 
 docker-compose -f config/services.yml pull
+docker-compose -f config/db-setup.yml pull
 docker-compose -f config/overview.yml pull
 
