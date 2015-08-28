e0bfab11e7ca3e5c7827c138ba974d596ba7d623
Jonas Karlsson <thejonas@gmail.com>
2015-08-28 16:58:28 -0400

    revert

--- windows/start-overview.sh
+++ windows/start-overview.sh
@@ -2,11 +2,11 @@
 
 
 ## Start services
-docker start overview-database
-docker start overview-messagebroker
-docker start overview-redis
-docker start overview-searchindex
-
+docker start \
+  overview-database \
+  overview-messagebroker \
+  overview-redis \
+  overview-searchindex
 
 ## Wait a few seconds to make sure database comes up
 ## then apply evolutions
@@ -25,16 +25,17 @@ docker run --name plugin-setup \
   overview/plugin-setup
 
 ## Start plugins
-docker start overview-word-cloud
-docker start overview-entity-filter
-docker start overview-grep
-docker start overview-file-browser
-docker start overview-multi-search
-
+docker start \
+  overview-word-cloud \
+  overview-entity-filter \
+  overview-grep \
+  overview-file-browser \
+  overview-multi-search
 
 ## Start Overview
-docker start documentset-worker
-docker start worker
-docker start web
+docker start \
+  documentset-worker \
+  worker \
+  web
 
 
