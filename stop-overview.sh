10a1e3abce7b284ce389703ee2d011aeac72a30e
Jonas Karlsson <thejonas@gmail.com>
2015-08-31 14:27:19 -0400

    Stop services in a nicer order

--- stop-overview.sh
+++ stop-overview.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker-compose -f config/services.yml stop
 docker-compose -f config/overview.yml stop
 docker-compose -f config/plugins.yml stop
+docker-compose -f config/services.yml stop
