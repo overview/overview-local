a3f63079639f856515a6de87c6384084e2fb0980
Adam Hooper <adam@adamhooper.com>
2015-11-09 17:41:20 -0500

    Make stop-overview.sh more forceful

--- stop-overview.sh
+++ stop-overview.sh
@@ -3,3 +3,6 @@
 docker-compose -f config/overview.yml stop
 docker-compose -f config/plugins.yml stop
 docker-compose -f config/services.yml stop
+docker-compose -f config/overview.yml rm -f
+docker-compose -f config/plugins.yml rm -f
+docker-compose -f config/services.yml rm -f
