96b0dde8b9072a6e13c313efdaeed7e1dba96519
Adam Hooper <adam@adamhooper.com>
2015-11-09 17:44:11 -0500

    Update everything we need

--- update-overview.sh
+++ update-overview.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-docker-compose -f config/services.yml stop
-docker-compose -f config/overview.yml stop
-
 docker-compose -f config/services.yml pull
-docker-compose -f config/db-setup.yml pull
 docker-compose -f config/overview.yml pull
-
+docker-compose -f config/plugins.yml pull
+docker pull overview/db-evolution-applier
+docker pull overview/plugin-setup
