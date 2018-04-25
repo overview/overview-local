cffe872c13046078152b2d7a361adad0b38b83e9
Adam Hooper <adam@adamhooper.com>
2018-04-25 16:57:31 -0400

    Update to converters

--- common.sh
+++ common.sh
@@ -13,5 +13,7 @@ set -e
 BUSYBOX_IMAGE="library/busybox:1.27.2"
 UBUNTU_IMAGE="library/ubuntu:17.10"
 
+eval "$(grep -v -E '^#|^OV_WELCOME_BANNER|^$' "$(dirname "$0")"/config/overview.defaults.env | sed 's/^/export /')"
+eval "$(grep -v -E '^#|^OV_WELCOME_BANNER|^$' "$(dirname "$0")"/config/overview.env | sed 's/^/export /')"
 DOCKER_COMPOSE="docker-compose -f $(dirname "$0")/config/overview.yml --project-name overview-local"
 grep -q -E '^OV_DOMAIN_NAME=.' "$(dirname "$0")/config/overview.env" && DOCKER_COMPOSE="$DOCKER_COMPOSE -f $(dirname "$0")/config/overview-ssl.yml" || true
