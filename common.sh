e58ea612033fd2750f359ff2da3794d8d63d57fa
Adam Hooper <adam@adamhooper.com>
2018-05-17 18:12:23 -0400

    s/overview-local/overviewlocal
    docker-compose just started treating slashes differently. _Correctly_,
    but differently. I hope we don't have too many users with the new
    version.

--- common.sh
+++ common.sh
@@ -15,5 +15,5 @@ UBUNTU_IMAGE="library/ubuntu:17.10"
 
 eval "$(grep -v -E '^#|^OV_WELCOME_BANNER|^$' "$(dirname "$0")"/config/overview.defaults.env | sed 's/^/export /')"
 eval "$(grep -v -E '^#|^OV_WELCOME_BANNER|^$' "$(dirname "$0")"/config/overview.env | sed 's/^/export /')"
-DOCKER_COMPOSE="docker-compose -f $(dirname "$0")/config/overview.yml --project-name overview-local"
+DOCKER_COMPOSE="docker-compose -f $(dirname "$0")/config/overview.yml --project-name overviewlocal"
 grep -q -E '^OV_DOMAIN_NAME=.' "$(dirname "$0")/config/overview.env" && DOCKER_COMPOSE="$DOCKER_COMPOSE -f $(dirname "$0")/config/overview-ssl.yml" || true
